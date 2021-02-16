9774c48834e2c2441228dcbabe54a67b8d22dc49
Adam Waern <awaern@gmail.com>
2021-02-16 15:05:24 +0100

    gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,22 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `I like Google fonts`
+  },
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `limelight`,
+          `source sans pro\:300,400,400i,700`,
+          `EB Garamond`,
+          `Quicksand\:600`// you can also specify font weights and styles
+        ],
+        display: "swap",
+      },
+    },
+  ],
 }
